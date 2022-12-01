f6d8c3c0fe85f775cca556ffa34536d160649f01
Luka Velinov <luka.velinov@gmail.com>
2022-12-01 20:36:40 +0100

    Add day 1, part 2

--- Day 01/part_2.py
+++ Day 01/part_2.py
@@ -1,5 +1,21 @@
+test_result = 24000
+
+
 def main():
-    return None
+    current_elf_calories = 0
+    calories = []
+
+    with open("Day 01/input.txt") as f:
+        for line in f:
+            if line.strip() == "":
+                calories.append(current_elf_calories)
+                current_elf_calories = 0
+            else:
+                current_elf_calories += int(line)
+
+    calories.sort()
+
+    return sum(calories[-3:])
 
 
 if __name__ == "__main__":
