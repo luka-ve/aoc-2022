725ecb51c9134803f149ac1f0431c0fd08cd129d
Luka Velinov <luka.velinov@gmail.com>
2022-12-02 21:50:37 +0100

    Add day 2, part 2

--- Day 02/part_1.py
+++ Day 02/part_1.py
@@ -3,44 +3,29 @@ import pandas as pd
 
 test_result = 15
 
-lose_to = {
-    'A': 'Y',
-    'B': 'Z',
-    'C': 'X',
-    'X': 'B',
-    'Y': 'C',
-    'Z': 'A'
-}
+lose_to = {"A": "Y", "B": "Z", "C": "X", "X": "B", "Y": "C", "Z": "A"}
 
-player_values = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3
-}
+player_values = {"X": 1, "Y": 2, "Z": 3}
 
-outcome_values = {
-    'lose': 0,
-    'draw': 3,
-    'win': 6
-}
+outcome_values = {"lose": 0, "draw": 3, "win": 6}
 
 
 def main(input_file):
     pairs = pd.read_csv(input_file, sep=" ", header=None, names=["elf", "player"])
 
     results = pairs.apply(get_ply_result, axis=1)
-    
+
     return results.sum()
 
 
 def get_ply_result(ply):
     play_points = 0
-    
+
     if lose_to[ply.elf] == ply.player:
-        play_points = outcome_values['win']
+        play_points = outcome_values["win"]
     elif lose_to[ply.player] == ply.elf:
-        play_points = outcome_values['lose']
+        play_points = outcome_values["lose"]
     else:
-        play_points = outcome_values['draw']
+        play_points = outcome_values["draw"]
 
     return play_points + player_values[ply.player]
--- Day 02/part_2.py
+++ Day 02/part_2.py
@@ -1,5 +1,22 @@
-test_result = None
+import pandas as pd
+from functools import reduce
+
+
+test_result = 12
 
 
 def main(input_file):
-    raise NotImplementedError
+    with open(input_file, "r") as f:
+        pairs = [line.strip().split(" ") for line in f]
+
+    play_values = {"X": 0, "Y": 1, "Z": 2, "A": 0, "B": 1, "C": 2}
+
+    outcomes = [0, 3, 6]
+
+    ply_results = map(
+        lambda pair: outcomes[play_values[pair[1]]]
+        + ((play_values[pair[0]] + play_values[pair[1]] - 1) % 3 + 1),
+        pairs,
+    )
+
+    return sum(ply_results)
