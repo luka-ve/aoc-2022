a6c7648e89b0578bae423693c8c796f45ef56644
Luka Velinov <luka.velinov@gmail.com>
2022-12-04 09:07:33 +0100

    Add day 4, part 2

--- Day 04/part_2.py
+++ Day 04/part_2.py
@@ -1,5 +1,21 @@
-test_result = None
+test_result = 6
 
 
 def main(input_file):
-    raise NotImplementedError
+
+    pairs = []
+
+    with open(input_file, "r") as f:
+        for line in f:
+            elves = line.strip().split(",")
+            sections = [[int(section) for section in elf.split("-")] for elf in elves]
+
+            pairs.append(sections)
+
+    return sum(
+        [
+            (p[0][0] <= p[1][1] and p[1][0] <= p[0][1])
+            or (p[1][0] <= p[0][1] and p[0][0] <= p[1][1])
+            for p in pairs
+        ]
+    )
